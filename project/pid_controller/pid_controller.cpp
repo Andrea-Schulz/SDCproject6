9e23076e5a961dee83300145ea34853b5b0db115
andrea.schulz <andrea.schulz@rwth-aachen.de>
2022-06-21 22:01:03 +0200

    step 1

--- project/pid_controller/pid_controller.cpp
+++ project/pid_controller/pid_controller.cpp
@@ -16,29 +16,39 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kpi, double Kii, double Kdi, double output_lim_maxi, double output_lim_mini) {
-   /**
-   * TODO: Initialize PID coefficients (and errors, if needed)
-   **/
+   //Initialize PID coefficients (and errors, if needed)
+   Kp = Kpi;
+   Ki = Kii;
+   Kd = Kdi;
+   output_lim_max = output_lim_maxi;
+   output_lim_min = output_lim_mini;
+   cte_curr = 0.0;
+
 }
 
 
 void PID::UpdateError(double cte) {
-   /**
-   * TODO: Update PID errors based on cte.
-   **/
+   cte_prev = cte_curr;
+   cte_curr = cte;
+   cte_int += cte * delta_time;
+
 }
 
 double PID::TotalError() {
-   /**
-   * TODO: Calculate and return the total error
-    * The code should return a value in the interval [output_lim_mini, output_lim_maxi]
-   */
-    double control;
+   //Calculate and return the total error
+   //The code should return a value in the interval [output_lim_mini, output_lim_maxi]
+
+    double control = -Kp*cte_curr - Kd*(cte_curr-cte_prev)/delta_time - Ki*cte_int;
+    if(control > output_lim_max){
+      control = output_lim_max;
+    }else if(control < output_lim_min){
+      control = output_lim_max;
+    }
     return control;
 }
 
 double PID::UpdateDeltaTime(double new_delta_time) {
-   /**
-   * TODO: Update the delta time with new value
-   */
+   //Update the delta time with new value
+   delta_time = new_delta_time;
+
 }
--- project/pid_controller/pid_controller.h
+++ project/pid_controller/pid_controller.h
@@ -14,50 +14,39 @@ public:
    * TODO: Create the PID class
    **/
 
-    /*
-    * Errors
-    */
+    //Errors
+    double cte_curr;
+    double cte_prev;
+    double cte_int;
 
-    /*
-    * Coefficients
-    */
+    //Coefficients
+    double Kp;
+    double Ki;
+    double Kd;
 
-    /*
-    * Output limits
-    */
+    //Output limits
+    double output_lim_max;
+    double output_lim_min;
   
-    /*
-    * Delta time
-    */
+    //Delta time
+    double delta_time;
 
-    /*
-    * Constructor
-    */
+    //Constructor
     PID();
 
-    /*
-    * Destructor.
-    */
+    //Destructor.
     virtual ~PID();
 
-    /*
-    * Initialize PID.
-    */
+    //Initialize PID.
     void Init(double Kp, double Ki, double Kd, double output_lim_max, double output_lim_min);
 
-    /*
-    * Update the PID error variables given cross track error.
-    */
+    //Update the PID error variables given cross track error.
     void UpdateError(double cte);
 
-    /*
-    * Calculate the total PID error.
-    */
+    //Calculate the total PID error.
     double TotalError();
   
-    /*
-    * Update the delta time.
-    */
+    //Update the delta time.
     double UpdateDeltaTime(double new_delta_time);
 };
 
